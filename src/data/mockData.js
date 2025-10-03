343d79ec5c0f10bdff7d7829517a0fc053ede34a
Your GitHub Username <desaiutkarsh542@gmail.com>
2025-10-03 13:20:39 +0530

    fix mock data

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,9 +1,9 @@
 export const timesheetData = [
-  { id: 1, Date: "1 - 5 January, 2024", status: "view" },
-  { id: 2, Date: "8 - 12 January, 2024", status: "update" },
-  { id: 3, Date: "15 - 19 January, 2024", status: "create" },
-  { id: 4, Date: "22 - 26 January, 2024", status: "view" },
-  { id: 5, Date: "29 Jan - 2 Feb, 2024", status: "update" }
+  { id: 1, week: 1, Date: "1 - 5 January, 2024", status: "completed", action: "view" },
+  { id: 2, week: 2, Date: "8 - 12 January, 2024", status: "missing", action: "update" },
+  { id: 3, week: 3, Date: "15 - 19 January, 2024", status: "incomplete", action: "create", },
+  { id: 4, week: 4, Date: "22 - 26 January, 2024", status: "completed", action: "view" },
+  { id: 5, week: 5, Date: "29 Jan - 2 Feb, 2024", status: "missing", action: "update" }
 ];
 
 export const userData = [
